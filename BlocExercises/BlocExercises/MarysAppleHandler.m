1f15262838f42e2d867aad7b18d178e0e625e4e5
Matt Lafferty <laff4545@yahoo.com>
2015-02-25 17:10:13 -0500

    #10 If/Else and Ternary Lesson

--- BlocExercises/BlocExercises/MarysAppleHandler.m
+++ BlocExercises/BlocExercises/MarysAppleHandler.m
@@ -11,22 +11,39 @@
 @implementation MarysAppleHandler
 
 - (NSString *) itemMaryCanPurchaseForDollars:(NSInteger)dollars {
-    NSString *itemToReturn;
-
-    /* WORK HERE */
     
-    NSLog(@"For $%ld, Mary can: %@", (long)dollars, itemToReturn);
+    NSInteger minimumDollars = 4;
+    NSString *getOut = @"I LOVE POTATOES";
+    
+    NSString *itemToReturn;
+    
+
+    if (dollars <= minimumDollars) {
+       getOut= (@"get out of my store");
+       return getOut;
+
+    }
+    
+    if (dollars < 6) {
+        itemToReturn = @"have some gum";
+    } else if (dollars < 1000) {
+        itemToReturn = @"have an apple";
+    } else if (dollars < 1000000000) {
+        itemToReturn = @"have an Apple computer";
+    } else {
+        itemToReturn = @"have The Big Apple";
+    }
+    
+    
     return itemToReturn;
+    
 }
 
 - (NSUInteger) dollarCostForAppleFlavoredVodka {
-    /* WORK HERE */
 
     NSUInteger cost = 24;
     
-    if (self.getsDiscount) {
-        cost *= .75;
-    }
+    cost = self.getsDiscount == YES ? cost * .75 : cost ;
     
     return cost;
 }
