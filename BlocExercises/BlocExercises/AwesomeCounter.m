fc9c15145bd61302a7de65c032b9bdc96f630be1
Matt Lafferty <laff4545@yahoo.com>
2015-03-01 17:31:41 -0500

    #11 Loops Lesson

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,30 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    
+    NSString *MyNumber = @"";
+
+    if (number < otherNumber){
+        while (number <= otherNumber){
+            
+            MyNumber = [MyNumber stringByAppendingFormat: @"%ld", number];
+            NSLog(@"%@", MyNumber);
+            number = number + 1;
+        }
+    }else if (number > otherNumber){
+        while (otherNumber <= number) {
+           
+            MyNumber = [MyNumber stringByAppendingFormat: @"%ld", otherNumber];
+            NSLog(@"%ld", (long) MyNumber);
+            otherNumber = otherNumber + 1;
+        }
+    }else if (number == otherNumber){
+            NSLog (@"%ld", (long) number);
+            MyNumber = [MyNumber stringByAppendingFormat: @"%ld", number];
+}
+    
+    return MyNumber;
+    
 }
 
 @end
